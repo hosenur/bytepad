52e7fad1bd974156d43f6b0fad12653b07b0e3dc
hosenur <mail@hosenur.dev>
2024-04-16 19:40:37 +0530

    init: prisma

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Sora } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs'
+const sora = Sora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Turborepo",
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={sora.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ apps/web/components/Hero.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useEffect } from "react";
+import {
+    useMotionTemplate,
+    useMotionValue,
+    motion,
+    animate,
+} from "framer-motion";
+
+const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
+
+export const Hero = () => {
+    const color = useMotionValue(COLORS_TOP[0]);
+
+    useEffect(() => {
+        animate(color, COLORS_TOP, {
+            ease: "easeInOut",
+            duration: 10,
+            repeat: Infinity,
+            repeatType: "mirror",
+        });
+    }, []);
+
+    const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
+    const border = useMotionTemplate`1px solid ${color}`;
+    const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
+
+    return (
+        <motion.section
+            style={{
+                backgroundImage,
+            }}
+            className="relative grid min-h-screen place-content-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200"
+        >
+            <div className="relative z-10 flex flex-col items-center">
+                <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-bold leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
+                    Your Code Slate Awaits
+                </h1>
+                <p className="my-6 max-w-2xl text-center text-base leading-relaxed md:text-lg md:leading-relaxed">
+                    Slatehash is an online playground for developers. React, Next.js, Nuxt ? We got you covered. Start coding now. Choose from a plethora of templates and get started.
+                </p>
+                <motion.button
+                    style={{
+                        boxShadow,
+                    }}
+                    whileHover={{
+                        scale: 1.015,
+                    }}
+                    whileTap={{
+                        scale: 0.985,
+                    }}
+                    className="group relative flex w-fit items-center gap-1.5 rounded-md bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
+                >
+                    Get Started
+                </motion.button>
+            </div>
+
+        </motion.section>
+    );
+};
