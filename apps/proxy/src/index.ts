43b5f3d05a1245225bb0d4870997c2ac8f543d43
hosenur <mail@hosenur.dev>
2024-04-21 00:36:27 +0530

    setup backend

--- apps/proxy/src/index.ts
+++ apps/proxy/src/index.ts
@@ -3,13 +3,20 @@ import proxyServer from "http-proxy";
 import { redis } from "./utils/redis";
 const proxy = proxyServer.createProxyServer({});
 const server = http.createServer(async (req, res) => {
-    const subdomain = req.headers.host?.split('.')[0];
-    if (!subdomain) {
-        return;
+    const host = req.headers.host;
+    if (host === 'hosenur.cloud') {
+        proxy.web(req, res, { target: 'http://10.122.0.2:8000' });
     }
-    const port = await redis.get(subdomain);
-    if (!port) {
-        return;
+    if (req.headers.host?.split('.')[1] === 'hosenur' && req.headers.host?.split('.').length === 3) {
+        const tag = req.headers.host?.split('.')[0];
+        const port = await redis.get(tag);
+        if (!port) {
+            return;
+        }
+        proxy.web(req, res, { target: 'http://10.122.0.2:' + port });
     }
-    proxy.web(req, res, { target: 'http://localhost:' + port });
+})
+
+server.listen(80, () => {
+    console.log("Proxy server is running on port 80");
 })
