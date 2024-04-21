bf365177ea08b14c2d2eb1e63fce77834a0516b0
hosenur <mail@hosenur.dev>
2024-04-22 00:31:24 +0530

    open origin

--- apps/web/src/hooks/useURL.tsx
+++ apps/web/src/hooks/useURL.tsx
@@ -1,5 +1,4 @@
 const useURL = (slug: string) => {
-    const url = process.env.NODE_ENV === 'development' ? `http://localhost:8080/${slug}` : `${import.meta.env.VITE_ENDPOINT_URL}/${slug}`;
-    return url;
+    return "https://hosenur.cloud/" + slug;
 }
 export { useURL };
