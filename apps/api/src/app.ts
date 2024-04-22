3238882f81cda0224d83d4c6e08b817fbfb8f709
hosenur <mail@hosenur.dev>
2024-04-23 00:35:54 +0530

    fixes

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -7,36 +7,49 @@ import { env } from "@/config";
 import { ClerkExpressWithAuth, LooseAuthProp } from "@clerk/clerk-sdk-node";
 
 export const app: Application = express();
+
 declare global {
     namespace Express {
         interface Request extends LooseAuthProp { }
     }
 }
+
+// Enable CORS for https://www.bytepad.pro
 app.use(
     cors({
         origin: "https://www.bytepad.pro",
     })
 );
+
+// Authenticate requests using Clerk
 app.all(
     "*",
     ClerkExpressWithAuth({
         signInUrl: "/login",
-    }));
+    })
+);
+
+// Helmet middleware for security headers
 app.use(
     helmet({
         contentSecurityPolicy: false,
     })
 );
+
+// Morgan middleware for logging
 app.use(morgan(env.isDev ? "dev" : "combined"));
+
+// Parse JSON requests
 app.use(
     express.json({
         limit: "5mb",
     })
 );
+
+// Define routes
 app.use(routes);
 
-
-
+// Handle 404 Not Found
 app.use("*", (_, res) => {
     res.status(404).json({
         message: "Not Found",
