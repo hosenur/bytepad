dab9b1ae015039aa700a8ee559abafe2e4d7eb03
hosenur <mail@hosenur.dev>
2024-04-22 21:08:42 +0530

    refactor: fetch backend url from env

--- apps/web/src/hooks/useURL.tsx
+++ apps/web/src/hooks/useURL.tsx
@@ -1,4 +1,5 @@
 const useURL = (slug: string) => {
-    return "https://hosenur.cloud/" + slug;
+    const url = process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://api.bytepad.pro";
+    return `${url}/${slug}`
 }
 export { useURL };
