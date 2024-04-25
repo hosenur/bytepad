63077d88df8cdad209fcfc86373739ce168c5e08
hosenur <mail@hosenur.dev>
2024-04-26 02:50:35 +0530

    xterm style fixes

--- apps/web/src/pages/Playground/Terminal.tsx
+++ apps/web/src/pages/Playground/Terminal.tsx
@@ -11,9 +11,8 @@ export default function Terminal({ tag, container, previewStatus }: { tag: strin
         if (!terminalRef.current || !tag || !container || !previewStatus) return;
 
         const term = new XTerm({
-            rows: 10,
             convertEol: true,
-            cursorStyle: 'underline',
+            cursorStyle: 'bar',
             cursorBlink: true,
             fontFamily: 'monospace',
             fontSize: 14,
